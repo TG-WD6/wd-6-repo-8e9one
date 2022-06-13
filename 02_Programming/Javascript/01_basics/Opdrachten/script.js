46baf56ad0652611bf013bdd9f272c4be7c45c16
Nathaniel Brunk <Nathaniel_Brunk@hotmail.com>
2022-06-13 16:16:39 +0200

    add opdrachten variables,string,conditions&functions

---
+++ 02_Programming/Javascript/01_basics/Opdrachten/script.js
@@ -0,0 +1,17 @@
+// let x = 2
+
+// if (x === 3) {
+//     console.log("you're number was 3")
+// } else if (x > 4) {
+//     console.log("you're number was greater than 4 but less than 12")
+// } else if (x > 11) {
+//     console.log("you're number was greater than 11")
+// } else if (x < 3) {
+//     console.log("you're number was less than 3")
+// }
+
+const inputData = document.getElementById("inputdata")
+
+function parameterFunction(parameter) {
+    console.log(parameter)
+}
