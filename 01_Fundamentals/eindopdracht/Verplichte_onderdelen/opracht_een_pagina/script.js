6a77ab628657027efec58acf8cccad862d511a52
Nathaniel Brunk <Nathaniel_Brunk@hotmail.com>
2022-06-14 09:18:22 +0200

    removed _colors.scss file

--- 01_Fundamentals/eindopdracht/Verplichte_onderdelen/opracht_een_pagina/script.js
+++ 01_Fundamentals/eindopdracht/Verplichte_onderdelen/opracht_een_pagina/script.js
@@ -1,17 +1,12 @@
+// pagination
 const prev = document.getElementById('prev')
 const next = document.getElementById('next')
 const pages = document.querySelectorAll('.page')
-const productVis = document.querySelectorAll('.product')
-const productEen = document.getElementById('product-een')
-const productTwee = document.getElementById('product-twee')
-const productDrie = document.getElementById('product-drie')
+
 // footer accordion
 const accordionToggle = document.querySelectorAll('.accordion-toggle')
 
-
 let currentActive = 1
-let limit = 3 * currentActive
-
 next.addEventListener('click', () => {
     currentActive++
 
@@ -19,8 +14,6 @@ next.addEventListener('click', () => {
         currentActive = pages.length
     }
     updatePage()
-    toggleVis()
-
 })
 
 prev.addEventListener('click', () => {
@@ -31,8 +24,6 @@ prev.addEventListener('click', () => {
 
     }
     updatePage()
-    toggleVis()
-
 })
 
 function updatePage() {
@@ -45,86 +36,10 @@ function updatePage() {
     })
 }
 
-function toggleVis() {
-    for (let i = 0; i < pages.length; i++) {
-        pages.style.visibility === "visible";
-        pages.style.maxHeight = null;
-
-    }
-}
-
-
 // footer accordion
 accordionToggle.forEach(accordtoggle => {
     accordtoggle.addEventListener('click', () => {
         accordtoggle.parentNode.classList.toggle('active')
     })
 })
-
-// function toggleVis() {
-//     if (currentActive === 1) {
-//         if (productEen.style.visibility === "hidden") {
-//
-//         } else {
-//             productEen.style.visibility === "hidden";
-//             productEen.style.maxHeight = 0;
-//         }
-//     } else if (currentActive === 2) {
-//         if (productTwee.style.visibility === "hidden") {
-//             productTwee.style.visibility === "visible";
-//             productTwee.style.maxHeight = null;
-//         } else {
-//             productTwee.style.visibility === "hidden";
-//             productTwee.style.maxHeight = 0;
-//         }
-
-//     } else if (currentActive === 3) {
-//         if (productDrie.style.visibility === "hidden") {
-//             productDrie.style.visibility === "visible";
-//             productDrie.style.maxHeight = null;
-//         } else {
-//             productDrie.style.visibility === "hidden";
-//             productDrie.style.maxHeight = 0;
-//         }
-
-//     }
-// }
-
-// function updateVisibility() {
-//     if (currentActive === 1) {
-//         if (productEen.style.maxHeight && productEen.style.opacity) {
-//             productEen.style.maxHeight = null;
-//             productEen.style.opacity = null;
-//         } else {
-//             productEen.style.maxHeight = productEen.scrollHeight + 'px'
-//             productEen.style.opacity = 1
-
-//         }
-//     } else if (currentActive === 2) {
-//         if (productTwee.style.maxHeight && productTwee.style.opacity) {
-//             productTwee.style.maxHeight = null;
-//             productTwee.style.opacity = null;
-//         } else {
-//             productTwee.style.maxHeight = productEen.scrollHeight + 'px'
-//             productTwee.style.opacity = 1
-
-//         }
-//     } else if (currentActive === 3) {
-//         if (productDrie.style.maxHeight && productDrie.style.opacity) {
-//             productDrie.style.maxHeight = null;
-//             productDrie.style.opacity = null;
-//         } else {
-//             productDrie.style.maxHeight = productEen.scrollHeight + 'px'
-//             productDrie.style.opacity = 1
-
-//         }
-//     }
-// }
-
-// if (productTwee.style.maxHeight && productTwee.style.opacity) {
-            //     productTwee.style.maxHeight = null;
-            //     productTwee.style.opacity = null;
-            // } else {
-
-            // }
 
