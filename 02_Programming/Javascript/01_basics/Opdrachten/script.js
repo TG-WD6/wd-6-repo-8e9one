3bab6a85fe3cb283ce7de76532e6e5fb7344d6f0
Nathaniel Brunk <Nathaniel_Brunk@hotmail.com>
2022-06-15 11:44:22 +0200

    opdracht 4/5

--- 02_Programming/Javascript/01_basics/Opdrachten/script.js
+++ 02_Programming/Javascript/01_basics/Opdrachten/script.js
@@ -10,8 +10,91 @@
 //     console.log("you're number was less than 3")
 // }
 
-const inputData = document.getElementById("inputdata")
+// let inputData = document.getElementById('inputData');
 
-function parameterFunction(parameter) {
-    console.log(parameter)
+// function parameterFunction(inputData) {
+//     console.log(`this is your input ${inputData}.`)
+// }
+
+// function functionHelloWorld() {
+//     console.log('HELLO WORLD')
+
+// }
+
+// let inputData = document.getElementById('inputData');
+
+// function parameterFunction(inputData) {
+//     console.log(`this is your input ${inputData.value}.`)
+//}
+
+// function randomNumber() {
+//     console.log(Math.random())
+// }
+// function randomNumber(x) {
+//     x = 10;
+//     console.log(Math.floor(Math.random() * x))
+// }
+
+// const group = ['Bob', 'luis', 'benn', 'john']
+
+// function luisInGroup() {
+
+//     if (group.includes('luis') === true) {
+//         console.log('Yes luis is in this group.')
+//     } else {
+//         console.log('no luis is not in this group.')
+//     }
+// }
+
+// const group = ['Bob', 'luis', 'benn', 'john']
+// let inputData = document.getElementById('inputData');
+
+// function parameterFunction(inputData) {
+//     console.log(`this is your input ${inputData.value}.`)
+//     if (group.includes(inputData.value) === true) {
+//         console.log('Yes luis is in this group.')
+//     } else {
+//         console.log('no luis is not in this group.')
+//     }
+// }
+// const arrayNummer = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// console.log(arrayNummer);
+
+// let arrayFruit = ['Appel', 'Aardbei', 'Druif', 'Bessen', 'Banaan']
+
+// console.log(arrayFruit);
+
+// console.log(arrayFruit[0], arrayFruit[1]);
+
+// console.log(arrayFruit[Math.floor(Math.random() * 5)]);
+
+// arrayFruit.splice(0, 1, 'Peer')
+
+// arrayFruit.splice(0, 2, 'Aardbei', 'Peer')
+
+// console.log(arrayFruit)
+
+// const dutchSports = ['Voetbal', 'Hockey', 'Schaatsen']
+
+// for (let i = 0; i < dutchSports.length; i++) {
+//     console.log(`index ${i} is hier ${dutchSports[i]}`)
+// }
+
+// for (let i = 0; i <= 20; i += 2) {
+//     console.log([i]);
+// }
+
+// for (let i = 1; i <= 3; i++) {
+//     console.log(`this is round ${i}`);
+//     for (let q = 1; q <= 10; q++) {
+//         console.log(q)
+//     }
+// }
+
+for (let i = 1; i <= 3; i++) {
+    console.log(`this is round ${i}`);
+    for (let q = 1; q <= 10; q++) {
+        console.log(q)
+    }
 }
