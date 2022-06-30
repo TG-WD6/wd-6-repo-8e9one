9cb8f11c67708dcb441c0524a852ef964c8ae29d
Nathaniel Brunk <Nathaniel_Brunk@hotmail.com>
2022-06-30 13:03:43 +0200

    carousel opdracht

---
+++ 02_Programming/Javascript/eindopdracht/03 Carousel/script.js
@@ -0,0 +1,39 @@
+const slider = document.querySelector('.container__slider');
+const carousel = document.querySelector('.container__carousel');
+
+const next = document.querySelector('.container__next');
+const prev = document.querySelector('.container__prev');
+
+let direction;
+
+next.addEventListener('click', function () {
+    carousel.style.justifyContent = 'flex-start';
+    direction = -1;
+
+    slider.style.transform = 'translate(-20%)';
+    console.log(direction)
+});
+
+prev.addEventListener('click', function () {
+    if (direction === -1) {
+        direction = 1;
+        slider.appendChild(slider.firstElementChild);
+    }
+    carousel.style.justifyContent = 'flex-end';
+    slider.style.transform = 'translate(20%)';
+    console.log(direction)
+});
+
+slider.addEventListener('transitionend', function () {
+    if (direction === 1) {
+        slider.prepend(slider.lastElementChild);
+    } else {
+        slider.appendChild(slider.firstElementChild);
+    }
+
+    slider.style.transition = 'none';
+    slider.style.transform = 'translate(0)';
+    setTimeout(function () {
+        slider.style.transition = 'all 0.5s';
+    })
+});
