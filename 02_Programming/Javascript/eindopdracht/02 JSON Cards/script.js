e6de57b91994cf60074cffcd6a093ddd6aebe933
Nathaniel Brunk <Nathaniel_Brunk@hotmail.com>
2022-07-04 20:49:09 +0200

    Manage to fetch from pokeapi

--- 02_Programming/Javascript/eindopdracht/02 JSON Cards/script.js
+++ 02_Programming/Javascript/eindopdracht/02 JSON Cards/script.js
@@ -8,6 +8,7 @@ const pokemonGen6 = 721;
 const pokemonGen7 = 809;
 const pokemonGen8 = 905;
 const pokemonGen9 = 913;
+
 // https://bulbapedia.bulbagarden.net/wiki/Category:Type_color_templates
 const colors = {
     bug: '#A8B820',
@@ -30,13 +31,106 @@ const colors = {
     steel: '#B8B8D0',
 }
 
-const fetchPokemons = async () => {
+const fetchGen1 = async () => {
     for (let i = 1; i <= pokemonGen1; i++) {
         await getPokemon(i)
     }
 }
-
-const getpokemon = async (id) => {
-    const src = `https://github.com/fanzeyi/pokemon.json/blob/master/pokedex.json`
-
+const fetchGen2 = async () => {
+    for (let i = 1; i <= pokemonGen2; i++) {
+        await getPokemon(i)
+    }
 }
+const fetchGen3 = async () => {
+    for (let i = 1; i <= pokemonGen3; i++) {
+        await getPokemon(i)
+    }
+}
+const fetchGen4 = async () => {
+    for (let i = 1; i <= pokemonGen4; i++) {
+        await getPokemon(i)
+    }
+}
+const fetchGen5 = async () => {
+    for (let i = 1; i <= pokemonGen5; i++) {
+        await getPokemon(i)
+    }
+}
+const fetchGen6 = async () => {
+    for (let i = 1; i <= pokemonGen6; i++) {
+        await getPokemon(i)
+    }
+}
+const fetchGen7 = async () => {
+    for (let i = 1; i <= pokemonGen7; i++) {
+        await getPokemon(i)
+    }
+}
+const fetchGen8 = async () => {
+    for (let i = 1; i <= pokemonGen8; i++) {
+        await getPokemon(i)
+    }
+}
+const fetchGen9 = async () => {
+    for (let i = 1; i <= pokemonGen9; i++) {
+        await getPokemon(i)
+    }
+}
+
+const getPokemon = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}/`
+    const res = await fetch(url)
+    const data = await res.json()
+    createPokemonInfo(data)
+}
+
+const createPokemonInfo = (pokemon) => {
+    const pokemonEl = document.createElement('div')
+    pokemonEl.classList.add('pokemon-container__pokemon')
+
+
+    const pokemonInnerHTML = `<div class="pokemon-container__pokemon-img">
+    <img src=https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png alt="">
+    </div>
+    <div class="pokemon-container__pokemon-info">
+        <div class="pokemon-container__primary-info">
+            <div>
+                <h3 class="pokemon-container__name">${pokemon.name}</h3>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"
+                alt="">
+                <span class="pokemon-container__number"># ${pokemon.id}</span>
+            </div>
+            <div class="pokemon-container__types">
+            <span class="pokemon-container__first-type">grass</span>
+            <span class="pokemon-container__second-type"> poison</span>
+        </div>
+    </div>
+    <div class="pokemon-container__secondary-info">
+        <div>
+            <div class="hp">HP: 60</div>
+            <div class="atk">ATK: 62</div>
+            <div class="def">DEF: 63</div>
+        </div>
+        <div>
+            <div class="speed">SPEED: 60</div>
+            <div class="sp-atk">SP.ATK: 80</div>
+            <div class="sp-def">SP.DEF: 80</div>
+        </div>
+    </div>
+    </div>`
+    console.log(pokemon.types)
+    pokemonEl.innerHTML = pokemonInnerHTML
+
+    pokemonContainer.appendChild(pokemonEl)
+}
+
+
+fetchGen1()
+// fetchGen2()
+// fetchGen3()
+// fetchGen4()
+// fetchGen5()
+// fetchGen6()
+// fetchGen7()
+// fetchGen8()
+
